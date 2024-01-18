22e1318339bbd87f632c7de6da24a130d3429e53
Krisandy Rivera <krisandy.rivera@binus.edu>
2024-01-19 04:52:18 +0700

    Set up navigation bar

--- app/src/main/java/com/example/mufiest/MainActivity.java
+++ app/src/main/java/com/example/mufiest/MainActivity.java
@@ -1,15 +1,86 @@
 package com.example.mufiest;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+
+import com.example.mufiest.controller.MovieController;
+import com.example.mufiest.controller.ReviewController;
+import com.example.mufiest.fragments.HomeFragment;
+import com.example.mufiest.fragments.MovieFragment;
+import com.example.mufiest.fragments.MovieScrollList;
+import com.example.mufiest.fragments.ReviewWithPosterList;
+import com.example.mufiest.models.Movie;
+import com.example.mufiest.models.ReviewWithDetail;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+
+        Drawable drawable = toggle.getDrawerArrowDrawable();
+        drawable.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+
+        if(savedInstanceState == null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content_container, new HomeFragment());
+            transaction.commit();
+            navView.setCheckedItem(R.id.home_menu);
+        }
+
+        toggle.syncState();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                transaction.replace(R.id.content_container, new HomeFragment());
+                transaction.commit();
+                navView.setCheckedItem(R.id.home_menu);
+                break;
+            case R.id.movies_menu:
+                transaction.replace(R.id.content_container, new MovieFragment());
+                transaction.commit();
+                navView.setCheckedItem(R.id.movies_menu);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 }
--- app/src/main/java/com/example/mufiest/fragments/HomeFragment.java
+++ app/src/main/java/com/example/mufiest/fragments/HomeFragment.java
@@ -1,10 +1,40 @@
-package com.example.mufiest;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+package com.example.mufiest.fragments;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mufiest.R;
+import com.example.mufiest.controller.MovieController;
+import com.example.mufiest.controller.ReviewController;
+import com.example.mufiest.models.Movie;
+import com.example.mufiest.models.ReviewWithDetail;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
 
 import com.example.mufiest.controller.MovieController;
 import com.example.mufiest.controller.ReviewController;
@@ -12,19 +42,27 @@ import com.example.mufiest.fragments.MovieScrollList;
 import com.example.mufiest.fragments.ReviewWithPosterList;
 import com.example.mufiest.models.Movie;
 import com.example.mufiest.models.ReviewWithDetail;
+import com.google.android.material.navigation.NavigationView;
 
-import java.util.ArrayList;
+public class HomeFragment extends Fragment {
 
-public class HomeActivity extends AppCompatActivity {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
         addPopularMovieList();
         addFavoriteMovieList();
         addRecentReviewsList();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     private void addRecentReviewsList() {
@@ -71,16 +109,15 @@ public class HomeActivity extends AppCompatActivity {
 
     private void addMovieScrollListFragment(ArrayList<Movie> movies, String movieListType, int fragmentId) {
         MovieScrollList movieScrollListFragment = MovieScrollList.newInstance(movies, movieListType);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(fragmentId, movieScrollListFragment);
         transaction.commit();
     }
 
     private void addReviewWithPosterScrollListFragment(ArrayList<ReviewWithDetail> reviews, String sortReviewBy, int fragmentId) {
         ReviewWithPosterList reviewWithPosterList = ReviewWithPosterList.newInstance(reviews, sortReviewBy);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(fragmentId, reviewWithPosterList);
         transaction.commit();
     }
-
 }
